83858ae4b4f510a6a9cbec11b3020c2251fc0dc1
Watanabeyouuu <1026740543@qq>
2021-08-31 21:30:58 +0800

    新增内容

---
+++ 1/1.16-1.30/connected_components/connected_components_labeling.py
@@ -0,0 +1,43 @@
+from skimage import measure
+import numpy as np
+import cv2
+
+plate = cv2.imread("license_plate.png")
+
+
+V = cv2.split(cv2.cvtColor(plate, cv2.COLOR_BGR2HSV))[2]
+thresh = cv2.adaptiveThreshold(V, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+	cv2.THRESH_BINARY_INV, 17, 3)
+
+cv2.imshow("License Plate", plate)
+cv2.imshow("Thresh", thresh)
+
+# 计算连通区域，8连通
+labels = measure.label(thresh, neighbors=8, background=0)
+mask = np.zeros(thresh.shape, dtype="uint8")
+print("[INFO] found {} blobs".format(len(np.unique(labels))))
+
+# 遍历所有连通域
+for (i, label) in enumerate(np.unique(labels)):
+	# 如果是背景，忽略
+	if label == 0:
+		print("[INFO] label: 0 (background)")
+		continue
+
+	# 展示当前的连通区域
+	print("[INFO] label: {} (foreground)".format(i))
+	labelMask = np.zeros(thresh.shape, dtype="uint8")
+	labelMask[labels == label] = 255
+	numPixels = cv2.countNonZero(labelMask)
+
+	# 判断区域是否满足面积要求
+	if numPixels > 300 and numPixels < 1500:
+		mask = cv2.add(mask, labelMask)
+
+	# 显示label
+	cv2.imshow("Label", labelMask)
+	cv2.waitKey(0)
+
+# 全部满足要求的连通域
+cv2.imshow("Large Blobs", mask)
+cv2.waitKey(0)
